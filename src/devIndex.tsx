cdb63283ad8f3989efd1c777845ec3d7eaddba09
Ouhan You <ouhan@numeric.io>
2025-10-06 17:11:33 -0700

    devIndex for local dev

---
+++ src/devIndex.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/globals.css';
+
+let rootEl: HTMLElement | null = document.getElementById('fdp-root');
+if (!rootEl) {
+  rootEl = document.createElement('div');
+  rootEl.id = 'fdp-root';
+  document.body.appendChild(rootEl);
+}
+const root = ReactDOM.createRoot(rootEl);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
 
 export const FDPApp = App;
-
-let rootEl: HTMLElement | null = document.getElementById('fdp-root');
-if (!rootEl) {
-  rootEl = document.createElement('div');
-  rootEl.id = 'fdp-root';
-  document.body.appendChild(rootEl);
-}
-const root = ReactDOM.createRoot(rootEl);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
