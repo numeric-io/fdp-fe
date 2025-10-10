cdae548b748685e9b00ee3822384f75002445c39
Ouhan You <ouhan@numeric.io>
2025-10-10 11:46:31 -0700

    export context provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ import App from './App';
 import './styles/globals.css';
 
 export const FDPApp = App;
+
+export * from './api-client/IBackendAPIClient';
+export { APIClientContext } from './App';
--- src/pages/POC.tsx
+++ src/pages/POC.tsx
@@ -1,10 +1,6 @@
 import RateCalculator from '@/components/ad-hoc/rateCalculator/RateCalculator';
 import UnmatchedEvents from '@/components/ad-hoc/UnmatchedEvents';
-import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { HealthCheckAPI } from '@numeric-io/fdp-api';
-import { useContext } from 'react';
-import { APIClientContext } from '../App';
 
 enum TabValue {
   UnmatchedEvents = 'unmatched-events',
@@ -12,15 +8,8 @@ enum TabValue {
 }
 
 export const POC = () => {
-  const apiClient = useContext(APIClientContext);
   return (
     <Tabs defaultValue={TabValue.RateCalculator} className="h-full">
-      <Button
-        onClick={async () => {
-          const res = await apiClient?.request(HealthCheckAPI, undefined);
-          console.log(res?.status);
-        }}
-      ></Button>
       <TabsList>
         <TabsTrigger value={TabValue.RateCalculator}>
           Rate Calculator
