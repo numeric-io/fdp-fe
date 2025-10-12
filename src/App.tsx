586218a6890b63d51110b6485a12c497b54ff710
Owen Arthur <owenarthurdesign@gmail.com>
2025-10-12 16:28:25 -0700

    coerce bad response from fdp api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { GetContractsAPI } from '@numeric-io/fdp-api'
-import { createContext, useCallback, useContext, useEffect } from 'react'
+import { createContext, useContext, useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { IBackendAPIClient } from './api-client/IBackendAPIClient'
-import { writeContracts } from './lib/store/stores/rateCalculator/write'
+import { fetchContracts } from './lib/store/stores/api'
 import POC from './pages/POC'
 
 interface AppContextType {
@@ -18,27 +17,9 @@ export const AppContext = createContext<AppContextType>({
 const App = () => {
   const { client } = useContext(AppContext)
 
-  const fetchContracts = useCallback(async () => {
-    try {
-      console.log('Fetching contracts...')
-      const contractsRes = await client?.request(GetContractsAPI, undefined)
-      console.log('Contracts response:', contractsRes)
-      if (!contractsRes) {
-        console.error('No contracts found')
-        return
-      }
-      console.log('Writing contracts to store:', contractsRes.contracts)
-      writeContracts(contractsRes.contracts)
-      console.log('Contracts written to store successfully')
-    } catch (error) {
-      console.error('Error fetching contracts:', error)
-    }
-  }, [client])
-
   useEffect(() => {
-    if (!client) return console.error('Client not found')
-    fetchContracts()
-  }, [client, fetchContracts])
+    fetchContracts(client)
+  }, [client])
 
   return (
     <Routes>
---
+++ src/lib/store/stores/api.ts
@@ -0,0 +1,15 @@
+import { IBackendAPIClient } from '@/api-client/IBackendAPIClient'
+import { GetContractsAPI } from '@numeric-io/fdp-api'
+import { Result } from '@numeric-io/result'
+import { Contract } from './rateCalculator/types'
+import { writeContracts } from './rateCalculator/write'
+
+export async function fetchContracts(client: IBackendAPIClient | null) {
+  if (!client) return console.error('Client not found')
+
+  const contractsRes = (await client.request(GetContractsAPI, undefined)) as unknown as Result<{
+    contracts: Contract[]
+  }>
+  if (!contractsRes || !contractsRes.ok) return console.error('No contracts found')
+  writeContracts(contractsRes.data.contracts)
+}
